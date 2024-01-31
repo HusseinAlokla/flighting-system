9f505a08c11e3a263c370bfe9d891b3d271cd445
HusseinAlokla <hussein77.alokla@gmail.com>
2024-01-31 14:15:11 +0200

    creating API to fetch all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,4 +20,47 @@ class UserController extends Controller
         // Handle the case where the user is not found
         // Redirect or return a response accordingly
     }
+    public function index(Request $request)
+    {
+        // $query = User::all();
+        // if ($query->count() > 0) {
+        //     return response()->json([
+        //         'status' => 200,
+        //         'users' => $query
+        //     ], 200);
+
+        // } else {
+        //     return response()->json([
+        //         'status' => 404,
+        //         'users' => 'No records found'
+        //     ], 404);
+        // }
+
+
+
+        $query = User::query();
+
+        // Filtering
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+        // Sorting
+        if ($request->has('sort_by')) {
+            $sortField = $request->input('sort_by');
+            $sortOrder = $request->input('sort_order', 'asc');
+            $query->orderBy($sortField, $sortOrder);
+        }
+
+        // Pagination
+        $perPage = $request->input('per_page', 10);
+        $users = $query->paginate($perPage);
+
+        if ($users->isEmpty()) {
+            return response()->json(['status' => 404, 'message' => 'No records found'], 404);
+        }
+
+        return response()->json(['status' => 200, 'users' => $users], 200);
+    }
+
 }
