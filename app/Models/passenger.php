c0f35a87cc47407814470625b598d12739fe954b
HusseinAlokla <hussein77.alokla@gmail.com>
2024-02-14 11:21:38 +0200

    adding pivot table

--- app/Models/passenger.php
+++ app/Models/passenger.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class passenger extends Model
+class Passenger extends Model
 {
     use HasFactory;
     use SoftDeletes;
