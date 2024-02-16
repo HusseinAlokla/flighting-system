55ffddacce38b79e5623730b747a3834e5787ec6
HusseinAlokla <hussein77.alokla@gmail.com>
2024-02-16 15:02:25 +0200

    implementing authentication using tokens

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FlightController;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,4 +27,25 @@ Route::get('flights', [FlightController::class, 'index']);
 
 
 Route::get('flights/{flight}/passengers', [FlightController::class, 'passengersByFlight']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->email)->plainTextToken;
+});
+Route::post('/users', [UserController::class, 'store']);
 
