d766e28bc6d893d3cff5d937ea64196d4d8f2dac
HusseinAlokla <hussein77.alokla@gmail.com>
2024-02-21 15:53:26 +0200

    removing unnecessary routes

--- routes/api.php
+++ routes/api.php
@@ -8,32 +8,26 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use App\Models\User;
 use App\Http\Controllers\AuthController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 
 });
+//CRUD for user
 Route::apiResource('users', UserController::class);
-Route::get('flights', [FlightController::class, 'index']);
+
+//CRUD for flights
+Route::apiResource('flights', FlightController::class);
 
 
-Route::get('flights/{flight}/passengers', [FlightController::class, 'passengersByFlight']);
-
-Route::create('/users', [UserController::class, 'store']);
 
 
-// Registration and Login
-Route::post('/register', [AuthController::class, 'register']);
+
+
+
+
+
+
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
