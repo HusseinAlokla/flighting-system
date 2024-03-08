f81de4e2654c00571360757884c09686e05b084a
HusseinAlokla <hussein77.alokla@gmail.com>
2024-03-08 15:06:17 +0200

    apply caching for passenger controller

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -4,24 +4,47 @@ namespace App\Http\Controllers;
 use App\Models\Passenger;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
-
+use Illuminate\Support\Facades\Cache;   
 class PassengerController extends Controller
 {
-    public function index(Request $request){
-    $passengers = QueryBuilder::for(Passenger::class)
-        ->with('flights') 
-        ->whereHas('flights', function ($query) use ($request) {
-            // Example conditional filter on the flights relationship
-            if ($request->has('flightName')) {
-                $query->where('name', $request->input('flightName'));
-            }
-        })
-        ->allowedFilters(['FirstName', 'LastName', 'email', 'DOB', 'passport_expiry_date'])
-        ->allowedSorts(['FirstName', 'LastName', 'email', 'DOB', 'passport_expiry_date'])
-        ->paginate($request->input('per_page', 10));
+    // public function index(Request $request){
+    // $passengers = QueryBuilder::for(Passenger::class)
+    //     ->with('flights') 
+    //     ->whereHas('flights', function ($query) use ($request) {
+    //         // Example conditional filter on the flights relationship
+    //         if ($request->has('flightName')) {
+    //             $query->where('name', $request->input('flightName'));
+    //         }
+    //     })
+    //     ->allowedFilters(['FirstName', 'LastName', 'email', 'DOB', 'passport_expiry_date'])
+    //     ->allowedSorts(['FirstName', 'LastName', 'email', 'DOB', 'passport_expiry_date'])
+    //     ->paginate($request->input('per_page', 10));
+
+    // return response(['success' => true, 'data' => $passengers]);
+    // }
+   
+
+public function index(Request $request)
+{
+    // Generate a unique cache key based on the request parameters
+    $cacheKey = 'passengers.index.' . md5(http_build_query($request->all()));
+
+    $passengers = Cache::remember($cacheKey, now()->addMinutes(60), function () use ($request) {
+        return QueryBuilder::for(Passenger::class)
+            ->with('flights') 
+            ->whereHas('flights', function ($query) use ($request) {
+                if ($request->has('flightName')) {
+                    $query->where('name', $request->input('flightName'));
+                }
+            })
+            ->allowedFilters(['FirstName', 'LastName', 'email', 'DOB', 'passport_expiry_date'])
+            ->allowedSorts(['FirstName', 'LastName', 'email', 'DOB', 'passport_expiry_date'])
+            ->paginate($request->input('per_page', 10));
+    });
 
     return response(['success' => true, 'data' => $passengers]);
-    }
+}
+
     public function create(Request $request)
     {
         $validatedData = $request->validate([
@@ -37,15 +60,27 @@ class PassengerController extends Controller
         $validatedData['password'] = bcrypt($validatedData['password']);
 
         $passenger = Passenger::create($validatedData);
-
+        Cache::forget('passengers.index'); 
         return response()->json($passenger, 201);
     }
 
     
+    // public function show(Passenger $passenger)
+    // {
+    //     return response()->json($passenger);
+    // }
     public function show(Passenger $passenger)
-    {
-        return response()->json($passenger);
-    }
+{
+    $cacheKey = 'passengers.show.' . $passenger->id;
+
+    $passengerData = Cache::remember($cacheKey, now()->addMinutes(60), function () use ($passenger) {
+        // If you need to load relationships or perform other operations, do so here.
+        return $passenger;
+    });
+
+    return response()->json($passengerData);
+}
+
 
     public function update(Request $request, Passenger $passenger)
     {
@@ -63,13 +98,17 @@ class PassengerController extends Controller
         }
 
         $passenger->update($validatedData);
-
+        Cache::forget('passengers.show.' . $passenger->id);
+        Cache::forget('passengers.index'); 
         return response()->json($passenger);
     }
 
     public function destroy(Passenger $passenger)
     {
         $passenger->delete();
+
+        Cache::forget('passengers.show.' . $passenger->id);
+        Cache::forget('passengers.index');
         return response(['success' => true, 'data' => $passenger]);
     }
 
